089668e62cb2c1de8ec8b74cbf513ba67a743740
Carlos Parra <eduardocarrizo18@gmail.com>
2023-05-02 15:59:43 -0400

    Correccion de estructura de Consulta de Sucursales Fix 2

--- app/Http/Controllers/API/UserSessionController.php
+++ app/Http/Controllers/API/UserSessionController.php
@@ -41,20 +41,20 @@ class UserSessionController extends Controller
     public function show($branch)
     {
       $sucursales = [
-        'VIG' =>  'value like \'%VL80%\'',
-        'SC'  =>  'value like \'%74\' or value like \'%39%\' or value like \'%63%\' or value like \'%64%\'',
-        'MRD' =>  'value like \'%VL75%\' or value like \'%VL76%\' or value like \'%VL77%\' or value like \'%VL78%\' or value like \'%VL79%\'',
-        'CLN' =>  'value like \'%VL72%\'',
-        'CLC' =>  'value like \'%VL66%\'',
-        'LFR' =>  'value like \'%VL67%\'',
-        'SA'  =>  'value like \'%VL81%\'',
-        'VLR' =>  'value like \'%VL61%\'',
-        'CJS' =>  'value like \'%VL65%\'',
-        'CCS' =>  'value like \'%3021%\'',
-        'BOC' =>  'value like \'%VL62%\'',
-        'MBO' =>  'value like \'%VL18%\' or value like \'%VL19%\' or value like \'%VL20%\' or value like \'%VL21%\'',
-        'MCH' =>  'value like \'%VL68%\'',
-        'PZO' =>  'value like \'%VL74-PZO\' or value like \'%VL92%\''
+        'VIG' =>  'value like \'%-VL80%\'',
+        'SC'  =>  'value like \'%-74\' or value like \'%-39%\' or value like \'%-63%\' or value like \'%-64%\'',
+        'MRD' =>  'value like \'%-VL75%\' or value like \'%-VL76%\' or value like \'%-VL77%\' or value like \'%-VL78%\' or value like \'%-VL79%\'',
+        'CLN' =>  'value like \'%-VL72%\'',
+        'CLC' =>  'value like \'%-VL66%\'',
+        'LFR' =>  'value like \'%-VL67%\'',
+        'SA'  =>  'value like \'%-VL81%\'',
+        'VLR' =>  'value like \'%-VL61%\'',
+        'CJS' =>  'value like \'%-VL65%\'',
+        'CCS' =>  'value like \'%-3021%\'',
+        'BOC' =>  'value like \'%-VL62%\'',
+        'MBO' =>  'value like \'%-VL18%\' or value like \'%-VL19%\' or value like \'%-VL20%\' or value like \'%-VL21%\'',
+        'MCH' =>  'value like \'%-VL68%\'',
+        'PZO' =>  'value like \'%-VL74-PZO\' or value like \'%-VL92%\''
       ];
 
       if(array_key_exists($branch, $sucursales))
